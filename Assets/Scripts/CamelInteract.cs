cdb436e0a4ca978e577be628e554f2608338d5fa
FlapJax06 <200616373@student.georgianc.on.ca>
2025-02-21 10:52:20 -0500

    W

--- Assets/Scripts/CamelInteract.cs
+++ Assets/Scripts/CamelInteract.cs
@@ -1,42 +1,55 @@
 using UnityEngine;
-//using Yarn.unity;
+using Yarn.Unity;
 
 public class CamelInteract : MonoBehaviour
 {
-    // refernece our runner
-    //public DialogueRunner dialogueRunner;
+    //refernece our runner
+    public DialogueRunner dialogueRunner;
+    public string NodeName;
 
     // boolean that determines if within the range in which the yarn script can be activated.
     private bool inzonetospeak = false;
 
-// Update is called once per frame
-    void CamelInteractionVoid()
+
+
+    void Start()
+
+    {
+
+        dialogueRunner = FindObjectOfType<DialogueRunner>();
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
     {
         if (inzonetospeak && Input.GetKeyDown(KeyCode.E))
         {
-            TalkToCamel();
+            dialogueRunner.StartDialogue(NodeName);
         }
 
     }
-    // start the dialogue runner based on name
-    void TalkToCamel()
-    {
-        if (inzonetospeak != null)
-        {
-            //dialogueRunner.StartDialogue(TalkToCamel);
-        }
-    }
+
 
     // TODO - use autocomplete to type this for you with correct syntax
-    //void OnTriggerEnter2D(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        inzonetospeak = true;
-    //    }
-    //    else (other.CompareTag("Player"))
-    //    {
-    //        inzonetospeak = false;
-    //    }
-    //}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inzonetospeak = true;
+        }
+
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                inzonetospeak = false;
+            }
+
+        }
+    }
 }
+
