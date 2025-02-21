b95817a224d6d5c1a312316ac51565c80d366b37
FlapJax06 <200616373@student.georgianc.on.ca>
2025-02-21 09:03:24 -0500

    add colliders

---
+++ Assets/Scripts/AnimalInteract.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Yarn.unity
+public class AnimalInteract : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
