1d16378bc49bdb1f768ce69b0af1a5fc72136afa
FlapJax06 <200616373@student.georgianc.on.ca>
2025-02-21 13:59:36 -0500

    4/4 signs done
    all signs done

---
+++ Assets/Scripts/PENGUINBUTTON.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PENGUINBUTTON : MonoBehaviour
+{
+    [SerializeField]
+    Button penguinbtn;
+
+    [SerializeField]
+    TMP_Text penguintxt;
+
+    void Start()
+    {
+        penguinbtn.onClick.AddListener(ButtonTest);
+    }
+
+    void ButtonTest()
+    {
+        penguintxt.text = "FUN FACTS ABOUT PENGUINS:\r\nPenguins can swim at speeds up to 10mph.\r\nPenguins are excellent divers and are capable of diving to depths of 800ft.\r\nPenguins are able to drink seawater.\r\nPenguins have no teeth.\r\nEmperor penguins incubate eggs with their feet.\r\nPenguins are waterproof!\r\nWhen they�re together in the water as a group, it�s called a raft\r\n";
+        Debug.Log("Penguin Button clicked");
+    }
+}
