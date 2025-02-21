3847afb35a146c6c7a68172fa8c242981ce36caa
FlapJax06 <200616373@student.georgianc.on.ca>
2025-02-21 13:42:40 -0500

    hopefully this works

--- Assets/Scripts/GOAT BUTTON.cs
+++ Assets/Scripts/GOAT BUTTON.cs
@@ -1,16 +1,26 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GOATBUTTON : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    Button horsebtn;
+
+    [SerializeField]
+    TMP_Text horsetxt;
+
     void Start()
     {
-        
+        horsebtn.onClick.AddListener(ButtonTest);
     }
 
-    // Update is called once per frame
-    void Update()
+    void ButtonTest()
     {
-        
+        horsetxt.text = "FUN FACTS ABOUT HORSES!\r\nHorses can sleep standing straight up.\r\nHorses CANNOT puke.\r\nIt can take anywhere from 9 months, to a year to regrow a full horse hoof.\r\nHorses produce approx. 10 gallons of saliva a day.\r\nHorses have 205 bones in the body, while people have 206.\r\nHorses have very large eyes\r\nThanks to their impressive vision and eye size, horses can see 350 degrees around them.\r\nHorses have 16 muscles in each ear.\r\nMost horses are actually born at night.\r\n\r\n";
+
+        Debug.Log("Horse Button clicked");
     }
 }
+
